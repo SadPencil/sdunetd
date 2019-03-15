11a1b5303a7b6e5f35f4dd73af61f54db6dded36
Sad Pencil <qh06@qq.com>
2019-03-15 16:16:40 +0000

    bug fix creating a file

--- console.go
+++ console.go
@@ -154,7 +154,7 @@ func cartman() {
 		if filename == "" {
 			filename = DEFAULT_CONFIG_FILENAME
 		}
-		f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
+		f, err := os.Create(filename)
 		defer f.Close()
 		if err != nil {
 			fmt.Println(err)
@@ -170,7 +170,7 @@ func cartman() {
 					fmt.Println(err)
 					saveFile = false
 				} else {
-					fmt.Println(`File saved. You may re-run the program with the "-c config" flag.`)
+					fmt.Println(`File saved. You may re-run the program with the "-config" flag.`)
 				}
 			}
 		}
