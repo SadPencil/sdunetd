5e8dd0657cf25330f452c3f84d62ceaed3b34e14
Sad Pencil <qh06@qq.com>
2019-08-09 11:29:52 +0800

    update version

--- versionsCurl.go
+++ versionsCurl.go
@@ -11,4 +11,4 @@ package main
 
 const NAME string = "sdunetd"
 const DESCRIPTION string = "Embedded SRUN3000 Portal Client for SDU-Qingdao"
-const VERSION string = "v2.2.1-curl"
+const VERSION string = "v2.2.2-curl"
